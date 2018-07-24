1384afdf1cad780131ed77f51f668694f800f464
daliyajohn <daliyajohn75@gmail.com>
2018-07-24 09:42:21 +0530

    Update menu

--- animated-menu/custom.js
+++ animated-menu/custom.js
@@ -1,4 +1,9 @@
 function toggle() {
-   var element = document.getElementById("menu-container");
-   element.classList.toggle("toggle-menu");
+  var element = document.getElementById('menu-container');
+  element.classList.toggle('toggle-menu');
+}
+
+function menuContent() {
+  var element = document.getElementById('banner');
+  element.classList.add('w3-animate-right');
 }
